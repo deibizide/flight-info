24e5183dafc8804dd5bfa48593845cc3f3592791
David <david.ramis89@gmail.com>
2020-01-29 21:11:42 +0100

    Updates

---
+++ precache-manifest.2220ee91d2404a757f6933ebcf58aee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c92d41cd9523ca835488c21a9aabf6",
+    "url": "/deibizide/flightInfo/index.html"
+  },
+  {
+    "revision": "bfffff559364f61461bc",
+    "url": "/deibizide/flightInfo/static/css/main.6aff9331.chunk.css"
+  },
+  {
+    "revision": "929b8efd926c5c27996d",
+    "url": "/deibizide/flightInfo/static/js/2.d39d7d73.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deibizide/flightInfo/static/js/2.d39d7d73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfffff559364f61461bc",
+    "url": "/deibizide/flightInfo/static/js/main.fc1b49f8.chunk.js"
+  },
+  {
+    "revision": "c3034c3e07c5f3aa2222",
+    "url": "/deibizide/flightInfo/static/js/runtime-main.fd02345c.js"
+  }
+]);
